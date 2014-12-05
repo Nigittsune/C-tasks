91e8ad5bb460d178fc1170a6f42f10ec014da2ce
feelthechockolate <dididavidova@gmail.com>
2014-12-05 08:34:19 +0200

    Revert "Revert "C tasks""
    This reverts commit 578b86882d35069f505cfd5c86c573b73a09ad22.

---
+++ billyears.zad2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    age = 59;
+    cout<<"Hi I`m Bill Gates and I am "<<age<<" years old";
+
+return 0;
+}
---
+++ hifmi.zad1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hi FMI! May the force be with you !";
+return 0;
+}
